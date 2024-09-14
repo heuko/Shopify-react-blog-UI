ce434601fa57547c647f6f672acea1db70aaed5f
Heroku5541 <heroku514@gmail.com>
2024-09-13 23:01:20 -0400

    remove push file

--- .rollup.cache/Users/harry/Documents/Shopify-react-blog-UI/pages/index.jsx
+++
@@ -1,4 +0,0 @@
-import BlogUI from '../components/BlogUI';
-var Home = function () { return (<BlogUI title="My First Blog" content="This is server-side rendered content!"/>); };
-export default Home;
-//# sourceMappingURL=index.jsx.map
